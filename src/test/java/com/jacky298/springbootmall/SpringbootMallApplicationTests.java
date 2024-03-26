9d765218085d6643002d6af9dfe65dd00597260f
jacky298 <jackychang298@gmail.com>
2024-03-26 16:34:51 +0800

    初始化Spring Boot project

---
+++ src/test/java/com/jacky298/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacky298.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
